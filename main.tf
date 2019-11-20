3a8711873afce7ff6d2cb0ff985b16297a5e44d3
Scott Brimhall <sbrimhall@salesforce.com>
2019-11-19 17:39:09 -0700

    Drop depends on

--- main.tf
+++ main.tf
@@ -35,7 +35,6 @@ resource "aws_vpc_peering_connection_accepter" "accepter" {
 
   vpc_peering_connection_id = aws_vpc_peering_connection.requester[0].id
   auto_accept               = true
-  depends_on                = [aws_vpc_peering_connection.requester]
 }
 
 resource "aws_vpc_peering_connection_options" "requester" {
